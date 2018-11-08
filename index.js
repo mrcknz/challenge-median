cdbe8cbe37c3f9d84acd24f49e5e0abf409358a0
Marco <kunz.marco@gmail.com>
2018-11-08 19:56:37 +0100

    Revert "solved?"

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 'use strict';
 
-function findMedian(a, b) {
-    if (a.length > 0 && a.length === b.length && typeof a[0] === 'number' ) {
-        var test_point = Math.floor(a.length / 2);
-        console.log(test_point);
-        return (a[test_point] + b[test_point]) / 2;
-    }
-    return false;
+function findMedian (arr1, arr2) {
+
 }
 
 exports.findMedian = findMedian;
